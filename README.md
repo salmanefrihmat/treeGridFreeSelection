ebf5ad41a76db8328af633dbea65b0156237da52
salmanefrihmat <sam.sensation@hotmail.fr>
2021-08-09 17:29:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-kgs75h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-kgs75h)
---
+++ index.ts
@@ -0,0 +1,46 @@
+import { TreeGrid, Selection } from '@syncfusion/ej2-treegrid';
+
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+
+let data: DataManager = new DataManager({
+  url:
+    'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData',
+  adaptor: new WebApiAdaptor(),
+  crossDomain: true
+});
+
+
+let treeGridObj: TreeGrid = new TreeGrid({
+  dataSource: data,
+  hasChildMapping: 'isParent',
+  idMapping: 'TaskID',
+  parentIdMapping: 'ParentItem',
+  pageSettings: { pageSize: 25 },
+  height: 535,
+  allowPaging: true,
+  treeColumnIndex: 1,
+
+
+  childMapping: 'subtasks',
+  selectionSettings: {
+    cellSelectionMode: 'Box',
+    type: 'Multiple',
+    mode: 'Cell'
+  },
+  height: 315,
+  treeColumnIndex: 1,
+  columns: [
+    { field: 'TaskID', headerText: 'Task ID', textAlign: 'Right', width: 90 },
+    { field: 'TaskName', headerText: 'Task Name', width: 180 },
+    {
+      field: 'StartDate',
+      headerText: 'Start Date',
+      textAlign: 'Right',
+      width: 90,
+      format: { skeleton: 'yMd', type: 'date' }
+    },
+    { field: 'Duration', headerText: 'Duration', width: 80, textAlign: 'Right' }
+  ]
+});
+
+treeGridObj.appendTo('#TreeGrid');
